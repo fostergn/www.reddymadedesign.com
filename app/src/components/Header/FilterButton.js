82f39ccc32966cc48ba9351bf8d89d1ebf4bf75c
Gavin Foster <gavinnfoster@gmail.com>
2017-10-05 06:37:57 -0500

    adding hamburger menu

--- app/src/components/Header/FilterButton.js
+++ app/src/components/Header/FilterButton.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import hamburger from '../../images/hamburger.svg';
 
 const FilterButton = ({workFilterOpen, toggleFilterMenu}) => {
 
-	const filterButton = workFilterOpen ? 'X' : 'Filter';
+	const filterButton = workFilterOpen ? 'X' : <img style={{height:14}} src={hamburger} />;
 
   return (
         <div className="header__filter-button" onClick={() => toggleFilterMenu()}>{filterButton}</div>
